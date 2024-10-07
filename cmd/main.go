d2dddaf168153896c88e6d59920044378e383f14
Erik <58817008+Erikqwerty@users.noreply.github.com>
2024-10-07 17:05:58 +0300

    implement idle logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	desc "github.com/erikqwerty/auth/pkg/UserAPI_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+const grpcPort = 50052
+
+type auth struct {
+	desc.UnimplementedUserAPIV1Server
+}
+
+func (a *auth) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Создание нового пользователя в системе: %v, %v, %v, %v, %v", req.Name, req.Email, req.Password, req.PasswordConfirm, req.Role)
+	return nil, nil
+}
+
+func (a *auth) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Получение информации о пользователе по его идентификатору: %v", req.Id)
+	return nil, nil
+}
+func (a *auth) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("Обновление информации о пользователе по его идентификатору %v", req)
+	return nil, nil
+}
+
+func (a *auth) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Eдаление пользователя из системы по его идентификатору: %v", req.Id)
+	return nil, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserAPIV1Server(s, &auth{})
+
+	log.Printf("server listening at :%v", grpcPort)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Faider to server: ", err)
+	}
 }
