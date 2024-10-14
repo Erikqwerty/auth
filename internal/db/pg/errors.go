ff4f0125207a6ca0ad850008891a5ead25b669e2
Erik <58817008+Erikqwerty@users.noreply.github.com>
2024-10-14 16:16:47 +0300

    немного сахар

--- internal/db/pg/errors.go
+++ internal/db/pg/errors.go
@@ -11,3 +11,13 @@ func errSQLtoSring(err error) error {
 func errSQLQwery(err error) error {
 	return fmt.Errorf("ошибка выполнения sql запроса: %w", err)
 }
+
+// Ошибка парсинга dsn для подключения к БД
+func errDSN(err error) error {
+	return fmt.Errorf("ошибка парсинга dsn: %w", err)
+}
+
+// Ошибка подключения к базе данных
+func errDBConect(err error) error {
+	return fmt.Errorf("ошибка подключения к database: %w", err)
+}
